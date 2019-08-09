9c3fb70219378038640612f8ff6a55e425a90ec5
zebravid <zebravid@gmail.com>
2019-08-09 11:36:48 +0300

    Gtts write sound files

---
+++ gttsSound.py
@@ -0,0 +1,18 @@
+from gtts import gTTS
+import os
+import json
+from support import selectionAct,timingLine,readFileListOfLines,writeToCsvExt,actorsListFill,objCreator
+
+
+data = []
+print("DTJSon")
+with open('output.jsonl') as f:
+	for line in f:
+		obj=(json.loads(line,object_hook=objCreator))
+		print("DTJSon")
+		print(obj)
+		data.append(obj)
+
+for itemT in data:
+	tts = gTTS(text=itemT.tex, lang='en')
+	tts.save("charId"+str(itemT.charId)+"lineNum"+str(itemT.lineNum)+"mp3")
